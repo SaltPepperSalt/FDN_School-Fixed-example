cd4bd3d746e3f796d7a400533eb8b94a255df7cd
SaltPepperSalt <dksro324@gmail.com>
2020-03-26 10:56:11 +0900

    git event 완성

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var menuLinks = $('.menu-link');
 var subAnchors = $('.submenu a');
 var listAnchors = $('.board ul li a');
 var boardHeading = $('.board section h2')
+var ulheight = parseInt($('.relsite-list').slideDown(0).css("height"));
+$("ulheight").css("height", "25px");
 
 subAnchors.attr('class', 'icon-dot-circled');
 listAnchors.attr('class', 'icon-dot-circled');
@@ -27,3 +29,6 @@ boardHeading.click(function (e) {
     $(this).parent().addClass('board-act');
     return false;
 });
+
+
+
